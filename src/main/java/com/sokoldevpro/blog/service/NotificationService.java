549cd311e270343caf14e1f8f0e213e062fcd749
Dmitry <dmitry.sokolovskyi@gmail.com>
2017-07-10 17:12:38 +0300

    added NotificationService and his impl, change PostController

---
+++ src/main/java/com/sokoldevpro/blog/service/NotificationService.java
@@ -0,0 +1,6 @@
+package com.sokoldevpro.blog.service;
+
+public interface NotificationService {
+    void addInfoMessage(String msg);
+    void addErrorMessage(String msg);
+}
--- src/main/java/com/sokoldevpro/blog/web/PostController.java
+++ src/main/java/com/sokoldevpro/blog/web/PostController.java
@@ -1,6 +1,7 @@
 package com.sokoldevpro.blog.web;
 
 import com.sokoldevpro.blog.domain.Post;
+import com.sokoldevpro.blog.service.NotificationService;
 import com.sokoldevpro.blog.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,10 +15,17 @@ public class PostController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private NotificationService notifyService;
+
     @RequestMapping("/posts/view/{id}")
-    public String view(@PathVariable("id") Long id, Model model) {
+    public String showViewPage(@PathVariable("id") Long id, Model model) {
         Post post = postService.findById(id);
+        if (post == null) {
+            notifyService.addErrorMessage("Cannot find post #" + id);
+            return "redirect:/";
+        }
         model.addAttribute("post", post);
-        return "posts/view";
+        return "posts/index";
     }
 }
